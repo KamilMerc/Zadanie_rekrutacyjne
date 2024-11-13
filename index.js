b6bd96248919a868c7eb104e77c535f9fe6eb8d6
Kamil <k.merc@op.pl>
2024-11-13 16:20:27 +0100

    Create instance of OpenAI, readArticleFromFile function and saveToFile function

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import OpenAI from 'openai/index.mjs';
 const fs = require('fs');
 require ('dotenv').config();
+
+const openAI = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function readArticleFromFile(path) {
+    return fs.promises.readFile(path, 'utf8');
+}
+
+async function saveToFile (htmlContent, path) {
+    return fs.promises.writeFile(path, htmlContent, 'utf8')
+}
